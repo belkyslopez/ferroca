905660043d5a91f575a5165eed440f2909cd3f68
LuisABF <luis.bascunan.f@gmail.com>
2022-08-17 22:53:10 -0400

    actualizacion

--- purchase-Order-Ferroca/src/app/core/config/url.services.ts
+++ purchase-Order-Ferroca/src/app/core/config/url.services.ts
@@ -1,1 +1,2 @@
-export const URL_SERVICIOS = "http://localhost:3978/api"
+//export const URL_SERVICIOS = "http://localhost:3978/api"
+export const URL_SERVICIOS = "https://product-jwt.herokuapp.com/api"
